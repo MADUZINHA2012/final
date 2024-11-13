ff15f575cccceb0ac0bf402bf4d0b78837004787
MADUZINHA2012 <00001092043950sp@al.educacao.sp.gov.br>
2024-11-13 08:35:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'O que é o Mapitoba?',
+    'A Mata da Matopiba é uma área de transição entre o Cerrado e a Floresta Amazônica, localizada nos estados do Maranhão, Tocantins, Piauí e Bahia'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que é Virtú e fortuna para Maquiavel?',
+    'a virtú dizia respeito às habilidades ou virtudes necessárias ao governante, a fortuna tratava-se da sorte, do acaso, da condição dada pelas circunstâncias da vida'
+)
+
+criaCartao(
+    'Sociologia',
+    'Qual é o conceito de Habitus para Bourdieu?',
+    'Ações que tomamos a partir da interiorização da classe que pertencemos '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
